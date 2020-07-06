cf762d2173afacc3ad398b19308e3c8414e9a222
Moksh Kant <mokshkant@icloud.com>
2020-07-07 01:56:43 +0530

    code fix

--- Greedy Algorithms/fractional_knapsack.py
+++ Greedy Algorithms/fractional_knapsack.py
@@ -1,49 +1,35 @@
 #fractional_knapsack
 M=float(input("Enter maximum Weight : "))
 n=int(input("Enter No of Objects : "))
-ww=[]
-vv=[]
-vw=[]
-for i in range(n):
-     a=float(input(" Enter Object Weight : "))
-     b=float(input(" Enter Respective Object Value : "))
-     ww.append(a)
-     vv.append(b)
-
-for i in range(n):
-    div=vv[i]/ww[i]
-    vw.append(div)
-
-sortvw = sorted(vw)
-sortvw.reverse()
-
+l=[]
 x=[]
 for i in range(n):
     x.append('0')
 
-w=[]
-v=[]
-
-for i in sortvw:
-    pos = vw.index(i)
-    w.append(ww[pos])
-    v.append(vv[pos])
-
-weight=0
-z=0
-
-while z<n and weight<M:
-    if weight+w[z]<=M:
-        x[z]=1
-    else :
-        x[z]=(M-weight)/w[z]
-
-    weight=weight+(x[z]*w[z])
-    z=z+1
-
-ans=0
-
 for i in range(n):
-    ans=ans+(x[i]*vv[i])
+     w=float(input(" Enter Object Weight : "))
+     v=float(input(" Enter Respective Object Value : "))
+     l.append((w,v))
+l.sort(key = lambda x: x[1]/x[0],reverse=True)
+#print(l)
+z=0
+weight=0
+res = [w[0] for w in l]
+res2 = [v[1] for v in l]
 
-print("Maximum value is:",ans)
+while z<n and weight<=M:
+    if weight+res[z]<=M:
+        x[z]=1.0
+    else:
+        x[z]=float((M-weight)/res[z])
+
+    weight=weight+(x[z]*res[z])
+    z=z+1
+#print(res2)
+#print(x)
+sum=0
+q=0
+while q<n:
+    sum=sum+(float(x[q])*float(res2[q]))
+    q=q+1
+print(sum)
